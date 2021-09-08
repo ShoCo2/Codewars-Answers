f2784b5cbcd2557b1dfe9a22629436cacd7829d8
ShoCo2 <coutu.calvin@gmail.com>
2021-09-07 23:15:38 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
